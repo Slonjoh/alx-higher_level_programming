bc33bf1841eaf01e68f49a7494d6c6333e3249e0
Slonjoh <tobson11122@gmail.com>
2023-10-03 15:25:41 -0700

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,36 +1,4 @@
 #!/usr/bin/python3
-
 def islower(c):
-    # Check if the character 'c' is lowercase
-    return ord('a') <= ord(c) <= ord('z')
-
-# Test cases
-print(islower("a"))  # True
-print(islower("A"))  # False
-print(islower("z"))  # True
-print(islower("Z"))  # False
-print(islower("0"))  # False
-
-# Additional test cases
-print(islower("4"))  # False
-print(islower("!"))  # False
-print(islower(4))    # False
-print(islower("H"))  # False
-
-# Check for non-alphabet characters
-if islower("a"):
-    print("'a' => lower")
-elif islower("A"):
-    print("'A' => upper")
-elif islower("4"):
-    print("'4' => upper")
-elif islower("!"):
-    print("'!' => upper")
-else:
-    print("Not a letter")
-
-# Check for non-string input
-try:
-    islower(4)
-except TypeError:
-    print("TypeError: islower() argument must be a string of length 1")
+    value = ord(c)
+    return value >= 97 and value <= 122)
