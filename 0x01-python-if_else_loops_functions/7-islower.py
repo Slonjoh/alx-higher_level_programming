18224c6dbebb50cb8445d5b5ce1945bae51f20b9
Slonjoh <tobson11122@gmail.com>
2023-10-03 15:18:25 -0700

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python3
 def islower(c):
-    # Check if the ASCII value of 'c' is between the ASCII values of 'a' and 'z'
     return ord('a') <= ord(c) <= ord('z')
 
 # Test cases
@@ -8,3 +8,4 @@ print(islower('A'))  # False
 print(islower('z'))  # True
 print(islower('Z'))  # False
 print(islower('0'))  # False
+
