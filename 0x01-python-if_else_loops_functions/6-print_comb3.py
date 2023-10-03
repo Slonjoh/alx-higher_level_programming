d24e983fe1bab6778424d8b6d9cd63c746d3916f
Slonjoh <tobson11122@gmail.com>
2023-10-03 14:52:57 -0700

    print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
+combinations = []
+
 for first_digit in range(10):
     for second_digit in range(first_digit + 1, 10):
         if first_digit != second_digit:
-            print("{:d}{:d}".format(first_digit, second_digit), end=", ")
-print()
+            combinations.append("{:d}{:d}".format(first_digit, second_digit))
+
+print(", ".join(combinations))
