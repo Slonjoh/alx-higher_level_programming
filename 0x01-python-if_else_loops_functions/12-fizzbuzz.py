3cd3dc8c0e81874d35a950e846b618fbeb89ea08
Slonjoh <tobson11122@gmail.com>
2023-10-03 15:55:47 -0700

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1, 101):
+        if (x % 3 == 0 and x % 5 == 0):
+            print("{}".format("FizzBuzz"), end=" ")
+        elif (x % 3 == 0):
+            print("{}".format("Fizz"), end=" ")
+        elif (x % 5 == 0):
+            print("{}".format("Buzz"), end=" ")
+        else:
+            print("{}".format(x), end=" ")
