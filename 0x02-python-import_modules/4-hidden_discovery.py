419aa8c33ebd1a9abb55ee3e654202155dd8aaf6
Slonjoh <tobson11122@gmail.com>
2023-10-05 11:50:31 -0700

    hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.8
+import hidden_4
+
+if __name__ == "__main__":
+    module_names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    sorted_names = sorted(module_names)
+    
+    for name in sorted_names:
+        print(name)
+
