cae2a24fc839fe08d9a8c8a3437bffc048b3eb66
Slonjoh <tobson11122@gmail.com>
2023-10-05 12:03:49 -0700

    codestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,10 @@
 import hidden_4
 
 if __name__ == "__main__":
-    module_names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    module_names = [
+        name for name in dir(hidden_4) if not name.startswith("__")
+    ]
     sorted_names = sorted(module_names)
-    
+
     for name in sorted_names:
         print(name)
-
