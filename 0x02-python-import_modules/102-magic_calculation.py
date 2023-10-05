e80ede641e5032ca537bb32bd7fd4139a68b8a43
Slonjoh <tobson11122@gmail.com>
2023-10-05 12:17:21 -0700

    magic calculator

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
     if a < b:
         add = a + b
         for i in range(4, 6):
-            add = add + i
+            add += add + i
         return (add)
     else:
         sub = a - b
