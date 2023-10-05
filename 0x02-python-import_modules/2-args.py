6636c434efc7c15b29d1853dcb8fd926b7f37076
Slonjoh <tobson11122@gmail.com>
2023-10-05 11:15:12 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+    args = argv[1:]
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print("1: {}".format(args[0]))
+    else:
+        print("{} arguments:".format(num_args))
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))
