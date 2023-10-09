d29938d5431534b561c12b7ae2fec8d865d65e05
Slonjoh <tobson11122@gmail.com>
2023-10-09 12:48:06 -0700

    maximum integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max_value = my_list[0]
+    if num > max_value:
+        max_value = num
+        return max_value
