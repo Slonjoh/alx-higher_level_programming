c6a49c753b86e2b1ce8fffc71d98b5561efe2b9a
Slonjoh <tobson11122@gmail.com>
2023-10-24 10:39:29 -0700

    Documentation

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,35 +1,67 @@
 #!/usr/bin/python3
+"""
+    This class represents a node in a singly linked list.
+"""
+
+
 class Node:
+    """
+    This class represents a node in a singly linked list.
+    """
     def __init__(self, data, next_node=None):
+        """
+        Init a new node with data and an optional reference to next node.
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """
+        Getter method for the data attribute.
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Setter method for the data attribute.
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """
+        Getter method for the next_node attribute.
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        Setter method for the next_node attribute.
+        """
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """
+    This class represents a singly linked list.
+    """
     def __init__(self):
+        """
+        Initializes an empty singly linked list.
+        """
         self.head = None
 
     def sorted_insert(self, value):
+        """
+        Inserts a new node into the list in sorted order.
+        """
         new_node = Node(value)
         if self.head is None or self.head.data >= value:
             new_node.next_node = self.head
@@ -42,6 +74,9 @@ class SinglyLinkedList:
             current.next_node = new_node
 
     def __str__(self):
+        """
+        Returns a string representation of the list.
+        """
         result = ""
         current = self.head
         while current is not None:
