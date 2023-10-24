f6f55a1bfe066f22714f84f7c6dfa437f77cdab1
Slonjoh <tobson11122@gmail.com>
2023-10-24 09:49:51 -0700

    Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""
+    This is the Square class.
+
+    It represents a square and initializes its size.
+"""
+
 
 class Square:
+    """
+    This is the Square class.
+
+    It represents a square and initializes its size.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new instance of the Square class with the given size.
+    """
     def __init__(self, size):
         self.__size = size
