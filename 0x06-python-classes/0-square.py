56f098e18e665f8aae2f744bd8fb9acc82e53118
Slonjoh <tobson11122@gmail.com>
2023-10-24 09:21:07 -0700

    documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,5 +3,13 @@
 class Square:
     """
     This is a simple Square class.
+
+    It can be used to represent a square and perform basic operations.
+
+    Attributes:
+        None
+
+    Methods:
+        None
     """
     pass
