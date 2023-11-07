88aecb9c8d701f3308895e9e268b7cd23223b617
Slonjoh <tobson11122@gmail.com>
2023-11-07 07:06:59 -0800

    JSON

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-"""initializate"""
+""" Module that contains a function that returns an object by
+a JSON representation
+"""
 import json
 
 
 def from_json_string(my_str):
-    """returns an object (Python data structure)
-     represented by a JSON string"""
+    """ Function that returns an object by a JSON representation
+
+    Args:
+        my_str: JSON representation
+
+    Raises:
+        Exception: when the string can't be decoded
+
+    """
     return json.loads(my_str)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""initializate"""
+""" Module that writes an Object to a text file using
+a JSON representation
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file"""
-    with open(filename, 'w') as f:
-        file_json = json.dumps(my_obj)
-        f.write(file_json)
-    f.close()
+    """ Function that writes an object to a text file
+    by a JSON representation
+
+    Args:
+        my_obj: object
+        filename: textfile name
+
+    Raises:
+        Exception: when the object can't be encoded
+
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
