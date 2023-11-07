8db698b45600c87e0cfa59c6e66490afafa5d4ea
Slonjoh <tobson11122@gmail.com>
2023-11-07 07:03:20 -0800

    JSON

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
+"""initializate"""
+import json
 
 
-"""
-That creates an Object from a “JSON file”.
-"""
-
-
-def load_from_json_file(filename):
-    """
-    That creates an Object from a “JSON file”.
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        return json.load(file)
+def from_json_string(my_str):
+    """returns an object (Python data structure)
+     represented by a JSON string"""
+    return json.loads(my_str)
