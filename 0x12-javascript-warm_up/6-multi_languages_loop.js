8ae5c1eb42a1ef854f5f240677a24839b1fbabd2
Slonjoh <tobson11122@gmail.com>
2024-01-08 12:27:16 -0800

    Advanced

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const myArr = [ 'C is fun', 'Python is cool', 'Javascript is amazing' ];
+const myArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 while (typeof (myArr[0]) !== 'undefined') {
   console.log(myArr.shift());
 }
