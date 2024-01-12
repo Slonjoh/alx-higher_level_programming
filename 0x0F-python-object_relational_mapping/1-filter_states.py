11ed86681e4bf1ef36301d2146859d76bbaf9bc6
Slonjoh <tobson11122@gmail.com>
2024-01-12 09:27:07 -0800

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,62 +1,30 @@
 #!/usr/bin/python3
-"""
-Script that lists all states with a name starting with
-N from the database hbtn_0e_0_usa.
-"""
-
-import MySQLdb
-import sys
+# Lists all states from a database
 
 if __name__ == "__main__":
-    """
-    Check if the correct number of arguments is provided
-    """
-    if len(sys.argv) != 4:
-        print("Usage: {:s} <username> <password> <database>"
-              .format(sys.argv[0]))
-        sys.exit(1)
-    """
-    Extracting MySQL connection details from command line arguments
-    """
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    import MySQLdb
+    from sys import argv, exit
 
-    """
-    Establish a MySQL connection
-    """
+    if len(argv) != 4:
+        print("Usage: {:s} <username> <password> <database>".format(argv[0]))
+        exit(1)
+
+    usr = argv[1]
+    pwd = argv[2]
+    dbe = argv[3]
+
     try:
-        conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database,
-            charset="utf8"
-        )
-        cursor = conn.cursor()
-
-        """
-        Execute the SELECT query for states starting with 'N'
-        """
-        cursor.execute("""
-            SELECT * FROM states WHERE name LIKE 'N%'
-            ORDER BY id ASC
-        """)
-        rows = cursor.fetchall()
-
-        """
-        Display results
-        """
-        for row in rows:
+        database = MySQLdb.Connect(user=usr, passwd=pwd, db=dbe, port=3306)
+    except Exception as err:
+        print(err)
+        exit(1)
+    cursor = database.cursor()
+    cursor.execute("""
+        SELECT * FROM states WHERE states.name LIKE 'N%'
+        ORDER BY states.id ASC
+    """)
+    for row in cursor.fetchall():
+        if row[1][0] == 'N':
             print(row)
-
-    except MySQLdb.Error as err:
-        print("Error connecting to MySQL: {}".format(err))
-        sys.exit(1)
-    finally:
-        """
-        Close the cursor and connection in the finally block
-        """
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
+    cursor.close()
+    database.close()
