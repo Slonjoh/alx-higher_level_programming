73da641347c62fae61914754e8e95df33f8af7f3
Slonjoh <tobson11122@gmail.com>
2024-01-12 09:31:06 -0800

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-# Lists all states from a database
+"""Select States Script to extract states from database table"""
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv, exit
-
-    if len(argv) != 4:
-        print("Usage: {:s} <username> <password> <database>".format(argv[0]))
-        exit(1)
-
-    usr = argv[1]
-    pwd = argv[2]
-    dbe = argv[3]
-
-    try:
-        database = MySQLdb.Connect(user=usr, passwd=pwd, db=dbe, port=3306)
-    except Exception as err:
-        print(err)
-        exit(1)
-    cursor = database.cursor()
-    cursor.execute("""
-        SELECT * FROM states WHERE states.name LIKE 'N%'
-        ORDER BY states.id ASC
-    """)
-    for row in cursor.fetchall():
-        if row[1][0] == 'N':
-            print(row)
-    cursor.close()
-    database.close()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    states = db.cursor()
+    states.execute("""SELECT * FROM states
+                   WHERE name
+                   LIKE BINARY 'N%'
+                   ORDER BY 'states.id' ASC""")
+    for state in states:
+            print("({}, '{}')".format(state[0], state[1]))
+    states.close()
+    db.close()
